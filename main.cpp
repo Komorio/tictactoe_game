2458771dfe09be52eab94a867ed89f25cb434e6e
Komorio <twilivez@gmail.com>
2022-11-13 04:13:16 +0900

    ✨ Make main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
-#include <iostream>
-
-using std::cout;
-using std::endl;
+#include "game/game.hpp"
+#include "object/object.hpp"
+#include "screen/screen.hpp"
+#include "vector2/vector2.hpp"
 
 int main() {
-  cout << "Hello World!" << endl;
+  Screen *screen = new Screen();
+  screen->clear();
+
+  Game *game = new Game(screen);
+
+  while (true) {
+    screen->render();
+    screen->update();
+
+    game->update();
+  }
+
+  delete screen;
+  delete game;
+
   return 0;
 }
